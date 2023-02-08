70f53be6244881a1ceb4d9b85dfb42f85906503f
cryscan <cryscan@umich.edu>
2023-02-08 01:25:18 -0500

    Contact force and control in state.

--- model.h
+++ model.h
@@ -21,13 +21,23 @@ namespace to {
     static constexpr int pose_state_dim = 7 + joint_space_dim;
     static constexpr int state_dim = 6 + joint_space_dim;
 
+    enum Side {
+        LEFT = 0,
+        RIGHT,
+        NONE
+    };
+
     struct State {
         using Scalar = Biped::rcg::Scalar;
 
         Biped::rcg::JointState q, qd, qdd;
         Eigen::Quaternion<Scalar> r;
-        Eigen::Vector3<Scalar> u, ud;     // Base angular velocity/acceleration (body frame)
-        Eigen::Vector3<Scalar> p, pd, pdd;// Base linear pose/velocity/acceleration (inertial frame)
+        Biped::rcg::Vector3 v, vd;     // Base angular velocity/acceleration (body frame)
+        Biped::rcg::Vector3 p, pd, pdd;// Base linear pose/velocity/acceleration (inertial frame)
+
+        Biped::rcg::JointState u;// Control
+        Biped::rcg::Vector3 f;   // Contact force
+        Side side = NONE;
 
         [[nodiscard]] auto state_vector() const {
             Eigen::Vector<Scalar, pose_state_dim> x;
@@ -38,7 +48,7 @@ namespace to {
         [[nodiscard]] auto base_body_velocity() const {
             Biped::rcg::Velocity xd;
             auto rot = r.inverse().toRotationMatrix();
-            xd << u, rot * pd;
+            xd << v, rot * pd;
             return xd;
         }
 
@@ -50,14 +60,14 @@ namespace to {
 
         [[nodiscard]] auto state_derivative() const {
             Eigen::Vector<Scalar, pose_state_dim> xd;
-            xd << body_to_quaternion_derivative(r, u).coeffs(), pd, qd;
+            xd << body_to_quaternion_derivative(r, v).coeffs(), pd, qd;
             return xd;
         }
 
         [[nodiscard]] auto base_acceleration() const {
             Biped::rcg::Acceleration xdd;
             auto rot = r.inverse().toRotationMatrix();
-            xdd << ud, rot * pdd;
+            xdd << vd, rot * pdd;
             return xdd;
         }
 
@@ -79,33 +89,22 @@ namespace to {
               jsim(inertia_properties, force_transforms) {
         }
 
-        enum Side {
-            LEFT = 0,
-            RIGHT
-        };
-
-        Eigen::Vector<Scalar, state_dim> dynamics_constraint(
-            const State& state,
-            const Biped::rcg::JointState& control,
-            const Biped::rcg::Force& contact_force,
-            Side contact_side) {
+        Eigen::Vector<Scalar, state_dim> dynamics_constraint(const State& state) {
             using namespace Biped::rcg;
             Eigen::Vector<Scalar, state_dim> u;
 
-            u << Force::Zero(), control;
+            u << Force::Zero(), state.u;
             auto h = nonlinear_terms(state);
-            auto contact_jacobian = foot_jacobian(state, contact_side);
+            auto contact_jacobian = foot_jacobian(state).bottomRows<3>();
 
-            return jsim(state.q) * state.body_acceleration() + h - contact_jacobian.transpose() * contact_force - u;
+            return jsim(state.q) * state.body_acceleration() + h - contact_jacobian.transpose() * state.f - u;
         }
 
-        Biped::rcg::Force contact_constraint(
-            const State& state,
-            Side contact_side) {
+        Biped::rcg::Vector3 contact_constraint(const State& state) {
             using namespace Biped::rcg;
 
-            auto contact_jacobian = foot_jacobian(state, contact_side);
-            auto contact_jacobian_derivative = foot_jacobian_time_derivative(state, contact_side);
+            auto contact_jacobian = foot_jacobian(state).bottomRows<3>();
+            auto contact_jacobian_derivative = foot_jacobian_time_derivative(state).bottomRows<3>();
 
             return contact_jacobian * state.body_acceleration() + contact_jacobian_derivative * state.state_derivative();
         }
@@ -120,10 +119,10 @@ namespace to {
         Biped::rcg::InverseDynamics inverse_dynamics;
         Biped::rcg::JSIM jsim;
 
-        Eigen::Matrix<Scalar, 6, state_dim> foot_jacobian(const State& state, Side side) {
+        Eigen::Matrix<Scalar, 6, state_dim> foot_jacobian(const State& state) {
             Eigen::Matrix<Scalar, 6, joint_space_dim> joint_space_jacobian = decltype(joint_space_jacobian)::Zero();
             Eigen::Vector4<Scalar> p{0, 0, 0, 1};
-            switch (side) {
+            switch (state.side) {
                 case Side::LEFT:
                     joint_space_jacobian.leftCols<3>() = jacobians.fr_trunk_J_L_foot(state.q);
                     p = homogeneous_transforms.fr_trunk_X_L_foot(state.q) * p;
@@ -131,6 +130,8 @@ namespace to {
                 case Side::RIGHT:
                     joint_space_jacobian.rightCols<3>() = jacobians.fr_trunk_J_R_foot(state.q);
                     p = homogeneous_transforms.fr_trunk_X_R_foot(state.q) * p;
+                    break;
+                case Side::NONE:
                     break;
             }
 
@@ -156,8 +157,9 @@ namespace to {
             state.r = Eigen::Quaternion<Scalar>(x(3), x(0), x(1), x(2));
             state.p = x.segment(4, 3);
             state.q = x.tail(joint_space_dim);
+            state.side = side;
 
-            return foot_jacobian(state, side);
+            return foot_jacobian(state);
         }
 
         Eigen::Vector<Scalar, state_dim> nonlinear_terms(const State& state) {
@@ -181,12 +183,12 @@ namespace to {
             return h;
         }
 
-        Eigen::Matrix<Scalar, 6, pose_state_dim> foot_jacobian_time_derivative(const State& state, Side side) {
+        Eigen::Matrix<Scalar, 6, pose_state_dim> foot_jacobian_time_derivative(const State& state) {
             Eigen::VectorX<Scalar> x(pose_state_dim), y(6);
 
             CppAD::Independent(x);
 
-            auto contact_jacobian = foot_jacobian(x, side);
+            auto contact_jacobian = foot_jacobian(x, state.side);
             y = contact_jacobian * state.body_velocity();
 
             CppAD::ADFun<double> fun;
@@ -212,16 +214,22 @@ namespace to {
             NodesRef base_angular_nodes,
             NodesRef base_linear_nodes,
             NodesRef joint_state_nodes,
+            NodesRef contact_force_nodes,
+            NodesRef control_nodes,
             const Scalar& dt)
             : model{std::move(model)},
               base_angular(std::move(base_angular_nodes), dt, 3),
               base_linear(std::move(base_linear_nodes), dt, 3),
-              joint_state(std::move(joint_state_nodes), dt, joint_space_dim) {}
+              joint_state(std::move(joint_state_nodes), dt, joint_space_dim),
+              contact_force(std::move(contact_force_nodes), dt, 3),
+              control(std::move(control_nodes), dt, joint_space_dim) {}
 
     private:
         Spline<Scalar, RotationAdaptor<Scalar>> base_angular;
         Spline<Scalar, Polynomial<Scalar>> base_linear;
         Spline<Scalar, Polynomial<Scalar>> joint_state;
+        Spline<Scalar, Polynomial<Scalar>> contact_force;
+        Spline<Scalar, Polynomial<Scalar>> control;
 
         ModelRef model;
     };
